da6a848d5ab5c114adb8803ae6d58f9871f5f8e5
wangx5318 <531881350@qq.com>
2021-05-20 01:46:52 +0800

    集成redis

--- src/main/java/com/example/demo/controller/CityController.java
+++ src/main/java/com/example/demo/controller/CityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -31,6 +32,13 @@ public class CityController {
     @GetMapping("/findCity")
     public List<City> findCity(){
         return cityService.findCity();
+    }
+
+    @ApiModelProperty("redis测试")
+    @GetMapping("/redisTest")
+    public String redisTest(){
+        Jedis jedis = new Jedis("localhost",6379);
+        return jedis.get("a");
     }
 
 }

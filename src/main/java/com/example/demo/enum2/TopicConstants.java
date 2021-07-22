395a1e89940c5150429f3c6367722cc8d94ce5ee
wangx5318 <531881350@qq.com>
2021-07-22 10:11:45 +0800

    删除章狗加的乱七八糟的

--- src/main/java/com/example/demo/enum2/TopicConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.enum2;
-
-public class TopicConstants {
-
-    public static final String EMAIL = "email";
-
-}
--- src/main/java/com/example/demo/service/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.ProductVo;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductRepository extends ElasticsearchRepository<ProductVo,String> {
-}
